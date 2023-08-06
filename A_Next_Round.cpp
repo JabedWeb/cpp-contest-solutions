58ef5f0b5f6cf6e7f6bf9b358a26fa25b46a256d
jabed366 <jabedhasan231@gmail.com>
2023-08-06 16:20:49 +0600

    A next round 158A problem solved

---
+++ A_Next_Round.cpp
@@ -0,0 +1,59 @@
+/*
+A. Next Round
+time limit per test3 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+"Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..." — an excerpt from contest rules.
+
+A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
+
+Input
+The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
+
+The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
+
+Output
+Output the number of participants who advance to the next round
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //here n is participant no ,
+    //k=ith place finishers score
+    int n,k;
+    cin>>n>>k;
+    vector <int> v;
+    while(n--){
+        int scores;
+        cin>>scores;
+        v.push_back(scores);
+    }
+    int count=0;
+    int th_place=v[k-1];
+
+
+    // is ith place scores is zero then
+    // then will make sure if there is one get positive value
+    if(th_place==0){
+        for(int i=0;i<v.size();i++){
+            if(v[i]>0){
+                count++;
+            }
+        }
+    }
+    //if ith place scores is not zero then
+    // then we will make sure who get less or equal to ith place scores
+    else {
+        for(int i=0;i<v.size();i++) {
+            if(th_place<=v[i]){
+                count++;
+        }
+    };
+    }
+    cout<<count<<endl;
+    return 0;
+}
