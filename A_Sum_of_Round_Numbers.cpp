06e90e197f68cd00d446eb090c1712dedd2aac37
Jabedweb <jabedhasan231@gmail.com>
2023-09-26 04:03:22 +0600

    A sum of round Numbers problem solved

--- A_Sum_of_Round_Numbers.cpp
+++ A_Sum_of_Round_Numbers.cpp
@@ -1,1 +1,30 @@
-#
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> roundNumbers(int n) {
+    vector<int> roundNums;
+    int divisor = 1;
+    while (n > 0) {
+        int remainder = n % 10;
+        if (remainder > 0) roundNums.push_back(remainder * divisor);
+        n /= 10;
+        divisor *= 10;
+    }
+    return roundNums;
+}
+
+int main() {
+    int t;cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+
+        vector<int> roundNums = roundNumbers(n);
+
+        cout << roundNums.size() << endl;
+        for (int i = 0; i < roundNums.size(); ++i) cout << roundNums[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
