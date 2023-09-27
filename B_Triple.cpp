2be37e286ac033f54e13a3ea2ad290a3745dfed9
Jabedweb <jabedhasan231@gmail.com>
2023-09-27 22:00:09 +0600

    Triple problem solved

--- B_Triple.cpp
+++ B_Triple.cpp
@@ -7,8 +7,22 @@ int main() {
    int t;cin>>t;
    while(t--){
     int n;cin>>n;
-    for(int i=0;i<n;i++){
-        
+      vector <int> count(n+1);
+      for(int i=1;i<=n;i++) count[i]=0;
+    
+    for(int i=1;i<=n;i++){
+      int x;cin>>x;
+      count[x]++;
     }
+   bool flag=false;
+    for(int i=1;i<=n;i++) {
+      if(count[i]>=3){
+         
+         cout<<i <<endl;
+         flag=true;
+         break;
+       }
+    };
+    if(!flag) cout<<-1<<endl;
    }
 }
