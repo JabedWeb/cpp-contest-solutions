747fcccc86daf2bcdea8c4cd5e62bb417b4a2e7f
Jabedweb <jabedhasan231@gmail.com>
2023-09-27 15:37:57 +0600

    Alesksa stack problem solved

---
+++ B_Triple.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/contest/1669/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int t;cin>>t;
+   while(t--){
+    int n;cin>>n;
+    for(int i=0;i<n;i++){
+        
+    }
+   }
+}
