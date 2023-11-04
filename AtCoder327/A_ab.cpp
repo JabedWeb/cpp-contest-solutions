6cd2e5a020da60baacde481ffb9d9ea6215a323f
Jabedweb <jabedhasan231@gmail.com>
2023-11-04 18:10:18 +0600

    ab find atcoder prob lem

---
+++ AtCoder327/A_ab.cpp
@@ -0,0 +1,21 @@
+//https://atcoder.jp/contests/abc327/tasks/abc327_a
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+    int n; cin >> n;
+    string s;cin>>s;
+    if(s.find("ab") !=string::npos || s.find("ba") !=string::npos) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+int main() {
+    solve();
+}
