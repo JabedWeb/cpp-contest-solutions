f8dbc95680f5145a49f178e0ca63a7edbc2b5f8b
Jabedweb <jabedhasan231@gmail.com>
2023-11-08 23:58:03 +0600

    implementation

---
+++ A_Secret_Sport.cpp
@@ -0,0 +1,17 @@
+
+////https://codeforces.com/contest/1894/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        string s; cin >> s;
+        cout << s[n-1] << endl;
+    }
+    return 0;
+}
