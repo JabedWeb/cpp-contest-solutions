68f1bdbc54864ce801196f796f4db0998016056d
Jabedweb <jabedhasan231@gmail.com>
2023-12-06 00:05:37 +0600

    learn how to use stack and time complexity

---
+++ B_YetnotherrokenKeoard.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s; cin >> s;
+    stack<int> lowerCasePositions, upperCasePositions;
+    string result = "";
+
+    for (char ch : s) {
+        if (ch == 'b') {
+            if (!lowerCasePositions.empty()) {
+                result[lowerCasePositions.top()] = '\0'; // Mark for deletion
+                lowerCasePositions.pop();
+            }
+        } else if (ch == 'B') {
+            if (!upperCasePositions.empty()) {
+                result[upperCasePositions.top()] = '\0'; // Mark for deletion
+                upperCasePositions.pop();
+            }
+        } else {
+            result += ch;
+            if (islower(ch)) {
+                lowerCasePositions.push(result.size() - 1);
+            } else if (isupper(ch)) {
+                upperCasePositions.push(result.size() - 1);
+            }
+        }
+    }
+
+    // Remove marked characters
+    result.erase(remove(result.begin(), result.end(), '\0'), result.end());
+    cout << result << endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+       solve();
+    }
+    return 0;
+}
