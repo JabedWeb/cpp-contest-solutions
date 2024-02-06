5c5612385dc88cf210ba179644465a8992d9d277
Jabedweb <jabedhasan231@gmail.com>
2024-02-06 22:31:51 +0600

    Problem solved

---
+++ A_Make_it_White.cpp
@@ -0,0 +1,31 @@
+//https://atcoder.jp/contests/abc326/tasks/abc326_c
+////https://codeforces.com/contest/266/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+        int n;
+        string s;
+        cin >> n >> s;
+
+        int first = s.find('B');
+        int last = s.rfind('B');
+        int length = (first == string::npos) ? 0 : last - first + 1;
+
+        cout << length << endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+       solve();
+    }
+    
+}
