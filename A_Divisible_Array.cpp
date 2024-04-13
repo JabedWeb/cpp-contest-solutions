ae556ed24459392a725b1f42106e13a6b220175a
Jabedweb <jabedhasan231@gmail.com>
2024-04-14 02:26:58 +0600

    concept divisible

---
+++ A_Divisible_Array.cpp
@@ -0,0 +1,29 @@
+//https://atcoder.jp/contests/abc326/tasks/abc326_c
+////https://codeforces.com/contest/266/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i=1;i<=n;i++){
+        cout << i *2 << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+}
