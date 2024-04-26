0787e002334ee08e022a31dbca62b3f2bf89c812
Jabedweb <jabedhasan231@gmail.com>
2024-04-27 01:32:52 +0600

    learned cycle shift

---
+++ A_Farmer_John_s_Challenge.cpp
@@ -0,0 +1,27 @@
+//https://atcoder.jp/contests/abc326/tasks/abc326_c
+////https://codeforces.com/contest/266/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+    int n,k; cin >> n >> k;
+    if(k==1 | k==n) {
+        for(int i=1; i<=n; i++) cout << k << " ";
+        cout << endl;
+    }
+    else cout << -1 << endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+       solve();
+    }
+}
