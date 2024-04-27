e21434d215726300caa27fe55dd99fb855c7a3a5
Jabedweb <jabedhasan231@gmail.com>
2024-04-27 14:58:25 +0600

    learned matrix

---
+++ A_Constructive_Problems.cpp
@@ -0,0 +1,24 @@
+//https://atcoder.jp/contests/abc326/tasks/abc326_c
+////https://codeforces.com/contest/266/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+    int n,m; cin >> n >> m;
+    cout <<min(n,m)+abs(n-m)<<endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    
+}
