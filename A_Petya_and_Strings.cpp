98dbb6b032c2cb1059a3254be78405876bdd71a3
Jabedweb <jabedhasan231@gmail.com>
2024-05-11 19:01:38 +0600

    problem number added

--- A_Petya_and_Strings.cpp
+++ A_Petya_and_Strings.cpp
@@ -1,14 +1,9 @@
-//https://atcoder.jp/contests/abc326/tasks/abc326_c
-////https://codeforces.com/contest/266/problem/A
+////https://codeforces.com/contest/112/problem/A
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 const int mod = 1e9 + 7;
 const int N = 1e5 + 5;
-
-int solve(int a,vector <int> &v){
-
-}
 
 void solve(){
     string s1,s2;cin>>s1>>s2;
