c8ca4a1b6d2371f3da612276b968b33f405679e5
Jabedweb <jabedhasan231@gmail.com>
2024-05-12 09:36:38 +0600

    Word capitalization easy problem

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,21 @@
+////https://codeforces.com/contest/281/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+    string s;cin>>s;
+    s[0]=toupper(s[0]);
+    cout<<s;
+
+}
+
+int main() {
+    solve();
+}
