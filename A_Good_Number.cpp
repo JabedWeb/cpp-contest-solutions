67ba559635c71824f760faa0a5a4bddafc017f17
Jabedweb <jabedhasan231@gmail.com>
2024-05-12 22:11:28 +0600

    learned modulo , set and  good problems for beginner

---
+++ A_Good_Number.cpp
@@ -0,0 +1,36 @@
+////https://codeforces.com/contest/365/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+int solve(int a,vector <int> &v){
+
+}
+
+void solve(){
+    int n,k;cin>>n>>k;
+    int ans=0;
+    while(n--){
+        int x;cin>>x;
+        set<int> s;
+        while(x){
+            s.insert(x%10);
+            x/=10;
+        }
+        bool flag=true;
+        for(int i=0;i<=k;i++){
+            if(s.find(i)==s.end()){
+                flag=false;
+                break;
+            }
+        }
+        if(flag) ans++;
+    }
+    cout<<ans;
+}
+
+int main() {
+    solve();
+}
