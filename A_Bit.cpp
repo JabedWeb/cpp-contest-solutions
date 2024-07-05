2076cb546410d91d5e62f7924a5fe965c5574bbd
Jabedweb <jabedhasan231@gmail.com>
2024-07-06 00:10:35 +0600

    string implementation

--- A_Bit.cpp
+++ A_Bit.cpp
@@ -5,23 +5,42 @@ typedef long long ll;
 const int mod = 1e9 + 7;
 const int N = 1e5 + 5;
 
-void solve(){
+void solve()
+{
 
-    int n;cin>>n;
+    int n;
+    cin >> n;
+    vector<string> str(n);
 
-    vector <string> str(n);
+    int plus = 0;
+    int minus = 0;
 
-    for(int i=0;i<n;i++) {
+    for (int i = 0; i < n; i++)
+    {
         string s;
-        cin>>s;
-        str[i]=s;
+        cin >> s;
+        str[i] = s;
+
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (s[j] == '+')
+            {
+                plus++;
+            }
+            else if (s[j] == '-')
+            {
+                minus++;
+            }
+        }
     }
+    plus = plus / 2;
+    minus = minus / 2;
 
-    
-
+    cout << plus - minus << endl;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     solve();
