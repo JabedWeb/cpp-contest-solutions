283c296b1b2af1688c346a05a54427cd683ddc34
Jabedweb <jabedhasan231@gmail.com>
2024-07-05 23:50:22 +0600

    string problem

---
+++ A_Bit.cpp
@@ -0,0 +1,28 @@
+////https://codeforces.com/problemset/problem/282/A
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+void solve(){
+
+    int n;cin>>n;
+
+    vector <string> str(n);
+
+    for(int i=0;i<n;i++) {
+        string s;
+        cin>>s;
+        str[i]=s;
+    }
+
+    
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
