96b5e52c632c0b354b0c3d4eb975241d91b5dfca
Jabedweb <jabedhasan231@gmail.com>
2024-07-10 03:14:07 +0600

    learned math implementation vid

--- A_Floor_Number.cpp
+++ A_Floor_Number.cpp
@@ -33,3 +33,21 @@ int main()
         solve();
     }
 }
+
+    //    int n, x; // n is the apartment number, x is apartments per floor from the second floor onwards
+    //     cin >> n >> x;
+
+    //     if (n <= 2) { // The first floor contains apartments 1 and 2
+    //         cout << 1 << endl;
+    //     } else {
+    //         int p = 2; // p is the highest apartment number on the current floor being considered
+    //         int a = 2; // a is the current floor number, starting from the second floor
+
+    //         // Iterate to find out on which floor apartment n would be
+    //         while (p + x < n) {
+    //             p += x; // Add x apartments to the highest number on the previous floor
+    //             a++;    // Move to the next floor
+    //         }
+
+    //         cout << a << endl; // Output the floor number
+    //     }
