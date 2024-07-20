059a6ee7eaf0e1350aeb0407517467bec4b4066f
Jabedweb <jabedhasan231@gmail.com>
2024-07-20 23:37:30 +0600

    fun with memory

---
+++ BIT/basic.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+void solve(){
+    // int n,p;cin>> n>>p;
+    // int product=1;
+    // for(int i=1;i<=p;i++){
+    //    product=product*n;
+    // }
+    // cout << product << endl;
+
+    long long int number;
+     // 2^31 =2147483648 this value is not print as it is out of range int value;
+    
+    // so if 2^31-1 value stored then it is worked 
+    cin >> number;
+    cout <<number << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+   solve();
+}
