3a24d65c754a47a047e16e6951c8352951ac24a9
Jabedweb <jabedhasan231@gmail.com>
2024-08-10 20:13:54 +0600

    last commit

--- BIT/basic.cpp
+++ BIT/basic.cpp
@@ -4,7 +4,8 @@ typedef long long ll;
 const int mod = 1e9 + 7;
 const int N = 1e5 + 5;
 
-void solve(){
+void solve()
+{
     // int n,p;cin>> n>>p;
     // int product=1;
     // for(int i=1;i<=p;i++){
@@ -13,15 +14,18 @@ void solve(){
     // cout << product << endl;
 
     long long int number;
-     // 2^31 =2147483648 this value is not print as it is out of range int value;
-    
-    // so if 2^31-1 value stored then it is worked 
+    // 2^31 =2147483648 this value is not print as it is out of range int value;
+
+    // so if 2^31-1 value stored then it is worked
     cin >> number;
-    cout <<number << endl;
+    cout << number << endl;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-   solve();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    solve();
 }
